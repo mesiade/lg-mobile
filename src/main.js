cdd52e403a29d7493bd76d2d86f99a2a9cf06553
mesiade <feather746@hotmail.com>
2021-05-20 17:42:50 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 将vant注册为Vue插件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
