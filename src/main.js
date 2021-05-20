96d3305519bf921d6f2f3e581c58d947d4015623
mesiade <feather746@hotmail.com>
2021-05-20 18:01:21 +0800

    安装amfe-flexible
    动态设置rem基准值

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// lib-flexible
+import 'amfe-flexible'
+
 // 引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
