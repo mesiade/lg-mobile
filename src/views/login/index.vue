ad3b66c2104d942d9493ce3bc3b7d934a9225ed1
mesiade <feather746@hotmail.com>
2021-05-23 16:15:16 +0800

    导航布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="登录"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  methods: {
+    onClickLeft () {
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
 
