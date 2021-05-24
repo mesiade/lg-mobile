ec075b923dc6fd885657324b81b55dd8a213aebe
mesiade <feather746@hotmail.com>
2021-05-24 12:07:49 +0800

    登录状态检测

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 import Course from '@/views/course'
 
@@ -19,12 +20,14 @@ const routes = [
   {
     path: '/learn',
     name: 'learn',
-    component: () => import(/* webpackChunkName: 'learn' */'@/views/learn')
+    component: () => import(/* webpackChunkName: 'learn' */'@/views/learn'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/user',
     name: 'user',
-    component: () => import(/* webpackChunkName: 'user' */'@/views/user')
+    component: () => import(/* webpackChunkName: 'user' */'@/views/user'),
+    meta: { requiresAuth: true }
   },
   {
     path: '*',
@@ -35,6 +38,26 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+// 设置导航守卫登录检测与跳转
+router.beforeEach((to, from, next) => {
+  // 验证to路由是否需要进行身份认证
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 验证 vuex store 中是否存储用户登录信息
+    if (!store.state.user) {
+      // 未登录，跳转到登录页面
+      next({
+        name: 'login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    }
+    next()
+  } else {
+    next()
+  }
 })
 
 export default router
