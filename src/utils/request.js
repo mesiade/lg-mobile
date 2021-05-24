3a63d59af7ec9070c96f8ff00434e1689dd74999
mesiade <feather746@hotmail.com>
2021-05-24 12:24:04 +0800

    通过请求拦截器设置token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   baseURL: 'http://edufront.lagou.com'
+})
+
+// 设置请求拦截器进行接口鉴权
+request.interceptors.request.use(config => {
+  const { user } = store.state
+  // 检测user是否存在
+  if (user && user.access_token) {
+    config.headers.Authorization = user.access_token
+  }
+  return config
 })
 
 export default request
