b0616914a539e85434a694b371b38d3669ae7fb7
mesiade <feather746@hotmail.com>
2021-05-24 11:31:10 +0800

    登录状态存储

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 存储登录用户数据
+    user: JSON.parse(window.localStorage.getItem('lagou-edu-mobile-user') || null)
   },
   mutations: {
+    // 修改user
+    setUser (state, payload) {
+      // payload为请求到的用户数据，JSON格式不方便操作，转换为对象存储
+      state.user = JSON.parse(payload)
+      // 本地存储
+      window.localStorage.setItem('lagou-edu-mobile-user', payload)
+    }
   },
   actions: {
   },
