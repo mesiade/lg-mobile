fabe9d96674a33a1744135fc4e44e38830ea64c9
mesiade <feather746@hotmail.com>
2021-05-26 17:18:26 +0800

    课程详情接口封装

--- src/views/course-info/index.vue
+++ src/views/course-info/index.vue
@@ -1,14 +1,31 @@
-<template>
+<template>  
   <div class="course-info">课程内容{{ courseId }}</div>
 </template>
 
 <script>
+import { getCourseById } from '@/services/course'
 export default {
   name: 'CourseInfo',
   props: {
     courseId: {
       type: [String, Number],
       required: true
+    }
+  },
+  data () {
+    return {
+      // 课程信息
+      course: {}
+    }
+  },
+  created () {
+    this.loadCourse()
+  },
+  methods: {
+    async loadCourse () {
+      const { data } = await getCourseById({
+        courseId: this.courseId
+      })
     }
   }
 }
