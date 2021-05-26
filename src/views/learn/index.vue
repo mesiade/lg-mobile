33f24aa84330d057b2d6b5e522a1c62ee97bd884
mesiade <feather746@hotmail.com>
2021-05-26 11:14:45 +0800

    学习功能布局

--- src/views/learn/index.vue
+++ src/views/learn/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="learn">学习</div>
+  <div class="learn">
+    <!-- 顶部功能 -->
+    <van-nav-bar title="已购课程" />
+    <!-- 课程列表 -->
+    <!-- 底部导航 -->
+    <layout-footer></layout-footer>
+  </div>
 </template>
 
 <script>
+import LayoutFooter from '@/components/LayoutFooter'
 export default {
-  name: 'Learn'
+  name: 'Learn',
+  components: {
+    LayoutFooter
+  }
 }
 </script>
 
