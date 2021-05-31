39a248e6d64fe7feb9eaebbd778f5ca5d8b17991
mesiade <feather746@hotmail.com>
2021-05-31 10:52:28 +0800

    图片压缩

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
+const loader = require("sass-loader")
+
 module.exports = {
   productionSourceMap: false, //不生成sourcemap
   css: {
     extract: false // 将css合并到js
+  },
+  chainWebpack: config => {
+    // 配置图片压缩
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        bypassOnDebug: true
+      })
+      .end()
   }
 }
